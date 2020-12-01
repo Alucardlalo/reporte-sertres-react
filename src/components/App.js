d7e7100c066fdd632d6bb87d0fb12426d7d2e64c
Eduardo Cuevas <eduardo.cuevas@sertres.com>
2020-12-01 17:13:32 -0600

    response to error

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Report from '../pages/Report';
 import ReportType from '../pages/ReportType';
 import ReportBody from '../pages/ReportBody';
 import One from './One';
+import notfound from '../pages/NotFound';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route exact path="/ReportType" component={ReportType}/>
                 <Route exact path="/Report" component={Report}/>
                 <Route exact path="/ReportBody" component={ReportBody}/>
+                <Route component={notfound}/>
             </Switch>
         </BrowserRouter>
     );
